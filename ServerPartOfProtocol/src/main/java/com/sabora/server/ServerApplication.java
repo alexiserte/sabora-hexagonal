7492e967f7ee6c628eb95c764859900456698a1a
alexiserte <pelechan@embl.de>
2024-10-20 20:49:40 +0200

    Probando cosas

--- ServerPartOfProtocol/src/main/java/com/sabora/server/ServerApplication.java
+++ ServerPartOfProtocol/src/main/java/com/sabora/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.sabora.server;
 
+import com.sabora.server.Socket.RepSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		RepSocket socket = new RepSocket(11434);
+		System.out.println("Hello World!");
+		socket.answerMessage();
 	}
 
 }
