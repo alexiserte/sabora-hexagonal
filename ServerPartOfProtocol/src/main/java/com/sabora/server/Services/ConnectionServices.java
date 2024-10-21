b80275345b9f91d097b94702060896b75b4c1ed2
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-10-21 19:21:34 +0200

    Esta a medias

---
+++ ServerPartOfProtocol/src/main/java/com/sabora/server/Services/ConnectionServices.java
@@ -0,0 +1,17 @@
+package com.sabora.server.Services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConnectionServices {
+
+
+    public ConnectionServices() {
+    }
+
+    public void addNewMobileConnectionm() {
+        ObjectMapper mapper;
+    }
+
+}
