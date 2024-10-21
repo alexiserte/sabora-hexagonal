cf423383d566dbb884c5266d04f2bf53d2cd5879
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-10-21 18:17:26 +0200

    Prueba

--- ServerPartOfProtocol/src/main/java/com/sabora/server/ServerApplication.java
+++ ServerPartOfProtocol/src/main/java/com/sabora/server/ServerApplication.java
@@ -1,6 +1,6 @@
 package com.sabora.server;
 
-import com.sabora.server.Socket.RepSocket;
+import com.sabora.server.Socket.ConnectionController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,7 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-		RepSocket socket = new RepSocket(11434);
+		ConnectionController socket = new ConnectionController(11434);
 		System.out.println("Hello World!");
 		socket.answerMessage();
 	}
