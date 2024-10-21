040b055e944e912742466a339a8d559e5d6f44ef
alexiserte <pelechan@embl.de>
2024-10-21 12:08:54 +0200

    Arreglando la chapuza de ayer

--- ServerPartOfProtocol/src/main/java/com/sabora/server/ServerApplication.java
+++ ServerPartOfProtocol/src/main/java/com/sabora/server/ServerApplication.java
@@ -3,15 +3,23 @@ package com.sabora.server;
 import com.sabora.server.Socket.RepSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-		RepSocket socket = new RepSocket(8080);
+		RepSocket socket = new RepSocket(11434);
 		System.out.println("Hello World!");
 		socket.answerMessage();
+	}
+	
+	@GetMapping("/")
+	public String hello(){
+		return "Bienvenido a Sabora!";
 	}
 
 }
