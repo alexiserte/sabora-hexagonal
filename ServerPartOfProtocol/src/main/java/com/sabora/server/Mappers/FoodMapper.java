27cf7cea0ed3d272b3fc610370402c7df28e324b
alexiserte <pelechan@embl.de>
2024-11-29 17:53:09 +0100

    Intentando arreglar bug en el Mapper

--- ServerPartOfProtocol/src/main/java/com/sabora/server/Mappers/FoodMapper.java
+++ ServerPartOfProtocol/src/main/java/com/sabora/server/Mappers/FoodMapper.java
@@ -4,7 +4,9 @@ import com.sabora.server.DTOs.FoodDTO;
 import com.sabora.server.Models.Food;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Service;
 
+@Service
 @Mapper(componentModel = "spring")
 public interface FoodMapper {
     @Mapping(target = "id", ignore = true)
