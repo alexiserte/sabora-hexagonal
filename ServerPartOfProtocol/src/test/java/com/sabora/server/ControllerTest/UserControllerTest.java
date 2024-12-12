90dc230d947eedfc178add4e042b2b490de81d7c
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-12-12 12:17:46 +0100

    Arreglando bugs en los test

--- ServerPartOfProtocol/src/test/java/com/sabora/server/ControllerTest/UserControllerTest.java
+++ ServerPartOfProtocol/src/test/java/com/sabora/server/ControllerTest/UserControllerTest.java
@@ -2,16 +2,16 @@ package com.sabora.server.ControllerTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private RestTemplate restTemplate;
