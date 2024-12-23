64189c9ad61aaa0e1ce0c96226f048164a711336
Alejandro Iserte <alexiserte@gmail.com>
2024-12-23 20:06:22 +0100

    Arreglando bugs

--- Server/src/main/java/com/sabora/server/Services/Implementation/FormServicesImplementation.java
+++ Server/src/main/java/com/sabora/server/Services/Implementation/FormServicesImplementation.java
@@ -1,9 +1,11 @@
 package com.sabora.server.Services.Implementation;
 
+import com.sabora.server.Configuration.EncryptionConfig;
 import com.sabora.server.DTOs.*;
 import com.sabora.server.Models.*;
 import com.sabora.server.Repositories.*;
 import com.sabora.server.Services.FormServices;
+import com.sabora.server.Utils.Encryption.DataEncryption;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
@@ -21,29 +23,35 @@ public class FormServicesImplementation implements FormServices {
     private QuestionOptionRepository questionOptionRepository;
     private QuestionServicesImplementation questionServices;
     private UserRepository userRepository;
+    private EncryptionConfig encryptionConfig;
 
 
-    public FormServicesImplementation(FormRepository formRepository, QuestionRepository questionRepository, AnswerRepository answerRepository, QuestionOptionRepository questionOptionRepository, QuestionServicesImplementation questionServices, UserRepository userRepository) {
+    public FormServicesImplementation(FormRepository formRepository, QuestionRepository questionRepository, AnswerRepository answerRepository, QuestionOptionRepository questionOptionRepository, QuestionServicesImplementation questionServices, UserRepository userRepository, EncryptionConfig encryptionConfig) {
         this.formRepository = formRepository;
         this.questionRepository = questionRepository;
         this.answerRepository = answerRepository;
         this.questionOptionRepository = questionOptionRepository;
         this.questionServices = questionServices;
         this.userRepository = userRepository;
+        this.encryptionConfig = encryptionConfig;
     }
 
     public FormDTO createFormDTO(Form form) {
-        int formId = form.getId();
-        String formName = form.getName();
-        String authorUsername = form.getAuthor().getDni();
-        Date creationDate = form.getDate();
+        try {
+            int formId = form.getId();
+            String formName = form.getName();
+            String authorUsername = DataEncryption.decrypt(form.getAuthor().getDni(), encryptionConfig.getSecretKey());
+            Date creationDate = form.getDate();
 
-        List<QuestionDTO> questionsDTO = new ArrayList<>();
-        List<Question> questions = questionRepository.findByFormId(formId);
-        for(Question question : questions) {
-            questionsDTO.add(questionServices.mapToQuestionDTO(question));
+            List<QuestionDTO> questionsDTO = new ArrayList<>();
+            List<Question> questions = questionRepository.findByFormId(formId);
+            for (Question question : questions) {
+                questionsDTO.add(questionServices.mapToQuestionDTO(question));
+            }
+            return new FormDTO(formId, formName, authorUsername, creationDate, questionsDTO);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return new FormDTO(formId, formName, authorUsername, creationDate, questionsDTO);
     }
 
 
@@ -52,14 +60,18 @@ public class FormServicesImplementation implements FormServices {
     }
 
     public void saveForm(FormDTO formDTO) {
-        Form form = new Form();
-        form.setAuthor((FoodSpecialist) userRepository.findByDni(formDTO.getFoodSpecialist()));
-        form.setName(formDTO.getName());
-        form.setDate(formDTO.getCreationDate());
 
-        form = formRepository.save(form);
+        try {
+            Form form = new Form();
 
-        for(QuestionDTO questionDTO : formDTO.getQuestions()) {
+            String dniEncrypted = DataEncryption.encrypt(formDTO.getFoodSpecialist(),encryptionConfig.getSecretKey());
+            form.setAuthor((FoodSpecialist) userRepository.findByDni(dniEncrypted));
+            form.setName(formDTO.getName());
+            form.setDate(formDTO.getCreationDate());
+
+            form = formRepository.save(form);
+
+            for(QuestionDTO questionDTO : formDTO.getQuestions()) {
                 Question question = questionServices.questionDTOToQuestion(questionDTO);
                 question.setForm(form);
                 questionRepository.save(question);
@@ -68,6 +80,9 @@ public class FormServicesImplementation implements FormServices {
                     option.setQuestion(question);
                     questionOptionRepository.save(option);
                 }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
