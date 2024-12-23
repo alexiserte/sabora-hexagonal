7eb2dbfb4a410fdc03cb33f265b0e65168769706
Alejandro Iserte <alexiserte@gmail.com>
2024-12-23 23:35:46 +0100

    Arreglando bug subida de archivos

--- Server/src/main/java/com/sabora/server/Clients/FileServiceClient.java
+++ Server/src/main/java/com/sabora/server/Clients/FileServiceClient.java
@@ -1,6 +1,7 @@
 package com.sabora.server.Clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,8 +9,9 @@ import org.springframework.web.multipart.MultipartFile;
 @FeignClient(name = "file-service", url = "http://localhost:8081")
 public interface FileServiceClient {
 
-    @PostMapping("/upload")
-    ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file);
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file);
+
 
     @DeleteMapping("/delete")
     ResponseEntity<String> deleteFile(@RequestParam("fileName") String fileName);
