b6348505b3144bab2674812047609e93a820f04d
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2025-01-23 19:57:16 +0100

    Servicio de estadisticas de proyectos vacio

---
+++ FormStatisticsService/src/main/java/com/sabora/FormStatisticsService/FormStatisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sabora.FormStatisticsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormStatisticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormStatisticsServiceApplication.class, args);
+	}
+
+}
---
+++ FormStatisticsService/src/test/java/com/sabora/FormStatisticsService/FormStatisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabora.FormStatisticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormStatisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
