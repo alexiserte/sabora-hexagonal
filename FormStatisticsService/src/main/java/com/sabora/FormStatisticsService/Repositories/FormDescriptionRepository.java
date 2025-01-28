ee594cca4097a76fd5f16d6f86ffa37b787c8198
Alejandro Iserte <alexiserte@gmail.com>
2025-01-28 17:08:02 +0100

    Servicio de analisis de respuestas con IA acabado de manera provisional

--- FormStatisticsService/src/main/java/com/sabora/FormStatisticsService/Repositories/FormDescriptionRepository.java
+++ FormStatisticsService/src/main/java/com/sabora/FormStatisticsService/Repositories/FormDescriptionRepository.java
@@ -4,5 +4,5 @@ import com.sabora.FormStatisticsService.Models.FormDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FormDescriptionRepository extends JpaRepository<FormDescription, Long> {
-    FormDescription findByFormId(String formId);
+    FormDescription findById(String formId);
 }
