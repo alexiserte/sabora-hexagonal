c68e93a971a4871353b2ef07f192135818db7dde
Alejandro Iserte <alexiserte@gmail.com>
2025-01-28 17:18:40 +0100

    Servicio de analisis de respuestas con IA acabado de manera provisional

---
+++ FormStatisticsService/src/main/java/com/sabora/FormStatisticsService/Configuration/RabbitMQConfig.java
@@ -0,0 +1,19 @@
+package com.sabora.FormStatisticsService.Configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue formularioCreadoQueue() {
+        return new Queue("formulario-creado", true);
+    }
+
+    @Bean
+    public Queue formularioActualizadoQueue() {
+        return new Queue("nueva-respuesta", true);
+    }
+}
