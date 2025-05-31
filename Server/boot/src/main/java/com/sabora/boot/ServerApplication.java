efddd081d0367b2452e5720a27d7703418f73c44
Alejandro Iserte <alexiserte@gmail.com>
2025-06-01 01:38:20 +0200

    feat: hexagonal skeleton

--- Server/boot/src/main/java/com/sabora/boot/ServerApplication.java
+++ Server/boot/src/main/java/com/sabora/boot/ServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sabora")
 @EntityScan(basePackages = {
 		"com.sabora.database.entities"
 })
