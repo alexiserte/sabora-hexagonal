2cf74b2fd4aefab4757fa4187765042cf29e5f12
Alejandro Iserte <alexiserte@gmail.com>
2025-06-01 22:36:19 +0200

    feat: openAPI code generation

--- Server/driven/mysql-repository/src/main/java/com/sabora/database/mappers/FormEntityMapper.java
+++ Server/driven/mysql-repository/src/main/java/com/sabora/database/mappers/FormEntityMapper.java
@@ -3,10 +3,14 @@ package com.sabora.database.mappers;
 import com.sabora.application.domain.Form;
 import com.sabora.database.entities.FormMO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {FoodSpecialistEntityMapper.class})
 public interface FormEntityMapper {
+
+
     FormMO toEntity(Form form);
 
+    @Mapping(target = "questions", ignore = true)
     Form toDomain(FormMO formMO);
 }
