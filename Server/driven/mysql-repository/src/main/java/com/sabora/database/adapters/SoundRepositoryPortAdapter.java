ac9275d4c88e234b4c6b4206d1cb397a3530f4ca
Alejandro Iserte <alexiserte@gmail.com>
2025-06-01 02:37:02 +0200

    feat: hexagonal skeleton

--- Server/driven/mysql-repository/src/main/java/com/sabora/database/adapters/SoundRepositoryPortAdapter.java
+++ Server/driven/mysql-repository/src/main/java/com/sabora/database/adapters/SoundRepositoryPortAdapter.java
@@ -4,7 +4,9 @@ import com.sabora.application.domain.Sound;
 import com.sabora.application.ports.driven.SoundRepositoryPort;
 import com.sabora.database.mappers.SoundEntityMapper;
 import com.sabora.database.repositories.SoundJpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class SoundRepositoryPortAdapter implements SoundRepositoryPort {
 
     private SoundJpaRepository soundJpaRepository;
