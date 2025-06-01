d51d748422fea06d6dc97872fb974b3e572135f0
Alejandro Iserte <alexiserte@gmail.com>
2025-06-01 02:42:00 +0200

    feat: hexagonal skeleton

--- Server/driven/mysql-repository/src/main/java/com/sabora/database/mappers/GlassesUserEntityMapper.java
+++ Server/driven/mysql-repository/src/main/java/com/sabora/database/mappers/GlassesUserEntityMapper.java
@@ -2,7 +2,9 @@ package com.sabora.database.mappers;
 
 import com.sabora.application.domain.GlassesUser;
 import com.sabora.database.entities.GlassesUserMO;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface GlassesUserEntityMapper {
     GlassesUserMO toEntity(GlassesUser glassesUser);
     GlassesUser toDomain(GlassesUserMO glassesUserMO);
