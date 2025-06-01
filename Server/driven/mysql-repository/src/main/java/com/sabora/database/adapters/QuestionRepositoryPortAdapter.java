bdd71c8c79006c0c1831fbb9fdec31eb84caacc9
Alejandro Iserte <alexiserte@gmail.com>
2025-06-01 23:17:16 +0200

    feat: openAPI code generation

--- Server/driven/mysql-repository/src/main/java/com/sabora/database/adapters/QuestionRepositoryPortAdapter.java
+++ Server/driven/mysql-repository/src/main/java/com/sabora/database/adapters/QuestionRepositoryPortAdapter.java
@@ -2,39 +2,40 @@ package com.sabora.database.adapters;
 
 import com.sabora.application.domain.Question;
 import com.sabora.application.ports.driven.QuestionRepositoryPort;
+import com.sabora.database.entities.QuestionMO;
 import com.sabora.database.mappers.QuestionEntityMapper;
 import com.sabora.database.repositories.QuestionJpaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 @AllArgsConstructor
 public class QuestionRepositoryPortAdapter implements QuestionRepositoryPort {
 
-    private QuestionJpaRepository questionJpaRepository;
-    private QuestionEntityMapper questionEntityMapper;
+    private final QuestionJpaRepository questionJpaRepository;
+    private final QuestionEntityMapper questionEntityMapper;
 
     @Override
     public Question findById(Integer id) {
-        return questionEntityMapper.toDomain(
-                questionJpaRepository.findById(id)
-        );
+        return questionEntityMapper.mapPolymorphicToDomain(questionJpaRepository.findById(id));
     }
 
     @Override
     public List<Question> findByFormId(Integer formId) {
-        return questionJpaRepository.findByFormId(formId)
-                .stream()
-                .map(questionEntityMapper::toDomain)
-                .toList();
+        List<com.sabora.database.entities.QuestionMO> entities = questionJpaRepository.findByFormId(formId);
+        return entities.stream()
+                .map(questionEntityMapper::mapPolymorphicToDomain)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Question save(Question question) {
-        return questionEntityMapper.toDomain(
-                questionJpaRepository.save(questionEntityMapper.toEntity(question))
-        );
+        com.sabora.database.entities.QuestionMO entity = questionEntityMapper.mapPolymorphicToEntity(question);
+        com.sabora.database.entities.QuestionMO savedEntity = questionJpaRepository.save(entity);
+        return questionEntityMapper.mapPolymorphicToDomain(savedEntity);
     }
 }
